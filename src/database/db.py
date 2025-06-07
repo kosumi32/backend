c4cfc95ac68f0fd261f23e2ef6006fea877e50c6
kosumi <eugeneq2632@gmail.com>
2025-06-07 19:24:29 +0800

    Done Database components

---
+++ src/database/db.py
@@ -0,0 +1,70 @@
+from sqlalchemy import Session
+from datetime import datetime, timedelta
+from . import models
+
+def get_challenge_quota(db: Session, user_id: str):
+    """
+    Get the challenge quota for a specific user.
+    If the user does not exist, create a new entry with default quota.
+    """
+    quota = (db.query(models.ChallengeQuota)    # specify what table/ model to query
+    .filter(models.ChallengeQuota.user_id == user_id)   # criteria
+    .first())
+    
+    if not quota:
+        # Create a new quota entry with default values
+        quota = models.ChallengeQuota(user_id=user_id, quota=15, last_reset_date= datetime.now())
+        db.add(quota)
+        db.commit()
+        db.refresh(quota)  # Refresh to get the updated object with ID
+    return quota
+
+
+def create_challenge_quota(db: Session, user_id: str):
+    """
+    Create a new challenge quota for a user.
+    This is used when the user is created.
+    """
+    quota = models.ChallengeQuota(user_id= user_id)
+    db.add(quota)
+    db.commit()
+    db.refresh(quota)  # Refresh to get the updated object with ID
+    return quota
+
+
+def reset_quota_if_needed(db: Session, quota: models.ChallengeQuota):
+
+    # Check if the last reset date is more than 24 hours ago
+    if datetime.now() - quota.last_reset_date > timedelta(hours=24):
+        quota.quota = 15  # Reset to default quota
+        quota.last_reset_date = datetime.now()  # Update the last reset date
+        db.commit()
+        db.refresh(quota)  # Refresh to get the updated object
+    return quota
+ 
+
+def create_challenge(db: Session, difficulty: str, created_by: str, title: str, options: str, correct_answer_id: int, explaination: str):
+    """
+    Create a new challenge in the database.
+    """
+    challenge = models.Challenge(
+        difficulty= difficulty,
+        created_by= created_by,
+        title= title,
+        options= options,
+        correct_answer_id= correct_answer_id,
+        explaination= explaination
+    )
+    db.add(challenge)
+    db.commit()
+    db.refresh(challenge)  # Refresh to get the updated object with ID
+    return challenge
+
+
+def get_user_challenges(db: Session, user_id: str):
+    """
+    Get all challenges created by a specific user.
+    """
+    challenges = db.query(models.Challenge).filter(models.Challenge.created_by == user_id).all()
+
+    return challenges
---
+++ src/database/models.py
@@ -0,0 +1,54 @@
+# Object Relational Mapping
+# Write python classes that represent a sequel table
+
+from sqlalchemy import Column, Integer, String, DateTime, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+
+# What DB we want to use
+engine= create_engine('sqlite:///database.db', echo=True)    # sqlite- run locally, echo=True to see SQL queries
+Base = declarative_base()
+
+# Create a class that represents a table
+# Inherit from Base
+class Challenge(Base):
+    __tablename__ = 'challenge'
+
+    id = Column(Integer, primary_key=True)
+    difficulty= Column(String, nullable=False)
+    date_created= Column(DateTime, default=datetime.now)
+    created_by= Column(String, nullable=False)  # UserID of the creator
+    title= Column(String, nullable=False)
+    options= Column(String, nullable=False)  # JSON string of options
+    correct_answer_id= Column(Integer, nullable=False)
+    explaination= Column(String, nullable=True) 
+
+    def __repr__(self):
+        return f"<User(id={self.id}, username='{self.username}', email='{self.email}')>"
+    
+
+class ChallengeQuota(Base):
+    __tablename__ = 'challenge_quota'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String, nullable=False, unique= True)  # userID should be unique
+    quota= Column(Integer, nullable=False, default= 15)  # Number of challenges created by the user
+    last_reset_date= Column(DateTime, default=datetime.now)  
+
+    def __repr__(self):
+        return f"<ChallengeQuota(id={self.id}, user_id='{self.user_id}', quota={self.quota})>"
+    
+
+# Converting to SQL code
+Base.metadata.create_all(engine)
+
+# Create a session to interact with the database
+Session = sessionmaker(autocommit= False, autoflush=False, bind=engine)
+
+def get_db():
+    db = Session()  # make sure dont create duplicate sessions
+    try:
+        yield db    # yield- generator function to return the session
+    finally:
+        db.close()
